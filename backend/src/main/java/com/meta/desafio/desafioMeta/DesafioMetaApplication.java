1d593f1d5b4d421acefb219603f58a43c5a263d7
dan9903 <mnssd2@gmail.com>
2021-02-10 09:39:41 -0300

    Creation of base backend project

---
+++ backend/src/main/java/com/meta/desafio/desafioMeta/DesafioMetaApplication.java
@@ -0,0 +1,13 @@
+package com.meta.desafio.desafioMeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioMetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioMetaApplication.class, args);
+	}
+
+}
